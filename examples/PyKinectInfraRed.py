968193e3d2bdafc78325a05999d9909e4b26111e
lbcwin <ohalesc@gmail.com>
2023-03-08 21:12:58 +0800

    test push

--- examples/PyKinectInfraRed.py
+++ examples/PyKinectInfraRed.py
@@ -1,18 +1,16 @@
-from pykinect2 import PyKinectV2
-from pykinect2.PyKinectV2 import *
-from pykinect2 import PyKinectRuntime
-
 import ctypes
-import _ctypes
-import pygame
 import sys
-import numpy as np
 
+import numpy as np
+import pygame
+
+from pykinect2 import PyKinectRuntime
+from pykinect2 import PyKinectV2
 
 if sys.hexversion >= 0x03000000:
-    import _thread as thread
+    pass
 else:
-    import thread
+    pass
 
 # colors for drawing different bodies 
 SKELETON_COLORS = [pygame.color.THECOLORS["red"],
